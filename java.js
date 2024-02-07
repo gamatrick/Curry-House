d5eabbe53ecfbbea2125fce3be75e40fb062254f
gamatrick <val@laflyfamily.com>
2024-02-07 13:37:42 +0100

    gznigne

---
+++ java.js
@@ -0,0 +1,13 @@
+offres.onclick = function() {
+    let start = Date.now();
+  
+    let timer = setInterval(function() {
+      let timePassed = Date.now() - start;
+  
+      offres.style.left = timePassed / 5 + 'px';
+  
+      if (timePassed > 2000) {
+        clearInterval(timer);
+      }
+    }, 20);
+  }
